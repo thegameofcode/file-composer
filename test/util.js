8b291d7d26324eda3bd5ba5f12acbd9d3aa69a5a
IGZJavierPerez <surtich@gmail.com>
2015-02-25 11:28:56 +0100

    Changed formatting style code: replaced tabs with spaces

--- test/util.js
+++ test/util.js
@@ -5,32 +5,32 @@ var render = require('../lib/file-composer').render;
 var fs = require('fs');
 
 function test(inputText, expectedOutputText, includeFiles, options) {
-	includeFiles = includeFiles || [];
-	
-	fs.readFileSync
-					.withArgs('input.txt', 'utf8')
-					.returns(inputText);
+  includeFiles = includeFiles || [];
 
-	for (var fileName in includeFiles) {
-		var contentFile = includeFiles[fileName];
-		fs.readFileSync
-						.withArgs(fileName, 'utf8')
-						.returns(contentFile);
-	}
+  fs.readFileSync
+          .withArgs('input.txt', 'utf8')
+          .returns(inputText);
 
-	fs.writeFile
-					.withArgs('out.txt', sinon.match.string, sinon.match.func);
+  for (var fileName in includeFiles) {
+    var contentFile = includeFiles[fileName];
+    fs.readFileSync
+            .withArgs(fileName, 'utf8')
+            .returns(contentFile);
+  }
 
-	options = options || {};
-	options.i = 'input.txt';
-	options.o = 'out.txt';
-	
-	render(options);
+  fs.writeFile
+          .withArgs('out.txt', sinon.match.string, sinon.match.func);
 
-	assert.equal(fs.writeFile.getCall(0).args[1], expectedOutputText);
+  options = options || {};
+  options.i = 'input.txt';
+  options.o = 'out.txt';
+
+  render(options);
+
+  assert.equal(fs.writeFile.getCall(0).args[1], expectedOutputText);
 
 }
 
 module.exports = {
-	test: test
+  test: test
 };
